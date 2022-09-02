dd454a886401da76035f563235963d2ff3657836
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-09-02 12:38:13 +0900

    installed a few stuff. still testing';

--- src/components/ShiftCalendar.vue
+++ src/components/ShiftCalendar.vue
@@ -1,102 +1,104 @@
 <template>
   <va-data-table :items="test">
     <template #cell(day1)="{ value }">
-      <va-input v-model="hey" placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day2)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day3)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day4)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day5)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day6)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
     <template #cell(day7)="{ value }">
-      <va-input placeholder="11-18" />
+      <hour-picker></hour-picker>
     </template>
   </va-data-table>
 </template>
 
-<script setup>
-  import { ref } from 'vue'
-  const hey = ref("")
-  const test = [
-    {
-      name: "ミナ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "チヅ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "チホ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "サヤカ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "アン",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "ケイゴ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-    {
-      name: "カンタ",
-      day1: "11-22",
-      day2: "11-23",
-      day3: "11-23",
-      day4: "11-23",
-      day5: "11-23",
-      day6: "11-23",
-      day7: "11-23",
-    },
-  ];
+<script setup lang="ts">
+import { ref } from "vue";
+import { HourPicker } from "@/scheduler/HourPicker";
+
+const hey = ref("");
+const test: Array<object> = [
+  {
+    name: "ミナ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "チヅ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "チホ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "サヤカ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "アン",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "ケイゴ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+  {
+    name: "カンタ",
+    day1: "11-22",
+    day2: "11-23",
+    day3: "11-23",
+    day4: "11-23",
+    day5: "11-23",
+    day6: "11-23",
+    day7: "11-23",
+  },
+];
 </script>
---
+++ src/components/scheduler/HourPicker.vue
@@ -0,0 +1,3 @@
+<template>
+  <va-input :max-length="5" placeholder="11-18" />
+</template>
