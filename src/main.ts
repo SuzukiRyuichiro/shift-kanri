143ee64d03138969089383b986dc37c99d98ead2
Ryuichiro Suzuki (Scooter) <46629248+SuzukiRyuichiro@users.noreply.github.com>
2022-09-14 00:16:59 +0900

    chore: add pinia (#2)
    * chore: install pinia
    
    * chore: add pinia and define some store
    
    * chore: add alias and expose and check in the devtools

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import { createVuesticEssential, VaDataTable, VaIcon, VaInput } from 'vuestic-ui'
 
 const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
 app.use(createVuesticEssential({ components: { VaDataTable, VaIcon, VaInput } }))
 app.mount('#app')
